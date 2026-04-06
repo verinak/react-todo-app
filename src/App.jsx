43dd65f1929f67a949ed4556ba2c8564848de966
verinak <verinamichelk@gmail.com>
2026-04-06 09:52:38 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <p className="text-center font-bold text-2xl">Todo App Home</p>
+        </>
+    );
+}
+
+export default App;

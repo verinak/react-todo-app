83478cb1a9343389ef5267d088c2af35548a62ca
verinak <verinamichelk@gmail.com>
2026-04-17 23:02:35 +0200

    feat(Profile): edit profile form to match home form layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { MdSave } from "react-icons/md";
+
 // import {
 //     FaUser,
 //     FaUserSecret,
@@ -11,61 +14,54 @@ function Profile() {
         return value || "Anonymous User";
     });
 
-    const [usernameInput, setUsernameInput] = useState(username);
-
-    useEffect(() => {
-        localStorage.setItem("username", JSON.stringify(username));
-    }, [username]);
-
     const handleUsernameChange = (event) => {
         // console.log("username change");
         // console.log(event.target.value);
-        setUsernameInput(event.target.value);
+        setUsername(event.target.value);
     };
 
     const handleUsernameSave = () => {
         // console.log("username save");
-        // console.log(usernameInput);
-        setUsername(usernameInput);
-        if (!usernameInput) setUsernameInput("Anonymous User");
+        localStorage.setItem("username", JSON.stringify(username === "Anonymous User" ? "" : username));
         alert("Username updated!");
     };
 
+    // todo: clear my data button or smth
     return (
         <div className="min-h-screen bg-slate-200 p-4 md:p-8 flex justify-center shadow-md">
             <div className="w-full max-w-md bg-white rounded-2xl p-6 space-y-6 h-fit">
                 {/* todo: user icons :( */}
                 {/* username input */}
-                <div className="flex flex-col md:flex-row gap-3">
+                <div className="flex flex-col md:flex-row gap-3 w-full relative">
+                    {/* todo: maybe consider using floating labels */}
                     <input
                         type="text"
-                        value={usernameInput}
+                        value={username}
                         onChange={handleUsernameChange}
                         placeholder="What should we call you?"
-                        className="flex-1 rounded-3xl border border-slate-300 px-5 py-2 outline-none focus:ring-2 focus:ring-slate-400 transition"
+                        className="w-full rounded-3xl border border-sky-950/40 bg-gray-50/80 px-4 py-2 pe-11
+                            outline-none focus:ring-1 focus:ring-sky-900/70 transition-all ease-in"
                     />
 
                     <button
-                        className="rounded-3xl px-5 py-2 bg-slate-600  hover:bg-slate-700 text-white font-medium transition"
+                        className="absolute end-0 top-1/2 mx-1 h-[85%] -translate-y-1/2 transform rounded-3xl p-2
+                            bg-sky-900/80 text-white enabled:hover:bg-sky-900/90 transition-all"
                         onClick={handleUsernameSave}
                     >
-                        Save
+                        {/* Save */}
+                        <MdSave className="h-full w-full" />
                     </button>
                 </div>
 
                 <div className="space-y-2 text-slate-800">
                     <p className="text-lg font-medium">
-                        You added{" "}
-                        <span className="font-bold">{savedTasks.length}</span>{" "}
-                        tasks so far.
+                        You added <span className="font-bold">{savedTasks.length}</span> tasks so far.
                     </p>
 
                     <p>
                         You completed{" "}
-                        <span className="font-bold">
-                            {savedTasks.filter((task) => task.completed).length}
-                        </span>{" "}
-                        of them. Good job!
+                        <span className="font-bold">{savedTasks.filter((task) => task.completed).length}</span> of them.
+                        Good job!
                     </p>
                 </div>
             </div>

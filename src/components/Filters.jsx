17b4c7a74978355d474d8e44c9b607e7992e13b7
verinak <verinamichelk@gmail.com>
2026-04-18 23:07:56 +0200

    feat(Filters): make filters collapsible on mobile

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { TbFilter, TbFilterX } from "react-icons/tb";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
 
 function Filters({ filters, sendFitlers }) {
-    {
-        /* todo: hide on mobile by default */
-    }
-    // const [showFilters, setShowFilters] = useState(false);
+    const [hideFilters, setHideFilters] = useState(false);
 
     // const [filters, setFilters] = useState({
     //     hideCompleted: false,
     //     // todo: priority filter
     //     // priority: "all",
     // });
+
+    const toggleFiltersMenu = () => {
+        setHideFilters((prev) => !prev);
+    };
 
     const handleFilterChange = (key, value) => {
         // console.log(event.target.id);
@@ -27,33 +29,43 @@ function Filters({ filters, sendFitlers }) {
     };
 
     return (
-        <div className="bg-slate-50 md:min-w-1/4 px-6 py-8 m-2 rounded-2xl text-sm md:text-base shadow-md">
-            {/* <button>
-                <TbFilter />
-            </button> */}
+        <div className="bg-slate-50 md:min-w-1/4 px-6 py-4 md:py-8 m-2 rounded-2xl text-sm md:text-base shadow-md">
+            <div className="flex">
+                <p className="flex-1 font-medium text-slate-700">Filters</p>
+                <button className="md:hidden" type="button" onClick={toggleFiltersMenu}>
+                    {hideFilters ? (
+                        <MdOutlineKeyboardArrowDown className="size-5 text-slate-700" />
+                    ) : (
+                        <MdOutlineKeyboardArrowUp className="size-5 text-slate-700" />
+                    )}
+                </button>
+            </div>
 
-            <p className="font-medium text-slate-700 mb-3">Filters</p>
+            <div
+                className={`transition-all duration-600 linear h-fit overflow-hidden
+                    ${hideFilters ? "max-h-0" : "max-h-100"}`}
+            >
+                <div className="my-4 space-y-2">
+                    <label className="flex items-center gap-3 cursor-pointer">
+                        <div className="relative">
+                            <input
+                                type="checkbox"
+                                className="peer sr-only"
+                                onChange={(event) => handleFilterChange("hideCompleted", event.target.checked)}
+                            />
 
-            <label className="flex items-center gap-3 cursor-pointer">
-                <div className="relative">
-                    <input
-                        type="checkbox"
-                        className="peer sr-only"
-                        onChange={(event) =>
-                            handleFilterChange(
-                                "hideCompleted",
-                                event.target.checked,
-                            )
-                        }
-                    />
+                            <div className="w-11 h-6 bg-slate-300 rounded-full peer-checked:bg-slate-700 transition"></div>
 
-                    <div className="w-11 h-6 bg-slate-300 rounded-full peer-checked:bg-slate-700 transition"></div>
+                            <div
+                                className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition
+                                    peer-checked:translate-x-5"
+                            ></div>
+                        </div>
 
-                    <div className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition peer-checked:translate-x-5"></div>
+                        <span className="text-slate-800">Hide completed tasks</span>
+                    </label>
                 </div>
-
-                <span className="text-slate-800">Hide completed tasks</span>
-            </label>
+            </div>
 
             {/* todo: reset filters button */}
         </div>
